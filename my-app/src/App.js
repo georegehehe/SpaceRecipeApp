296b36a608728811c4b171f90192c4bc17c33bf9
georegehehe <wangg@uchicago.edu>
2023-11-11 12:04:35 -0600

    connect frontend with backend

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,28 @@
 import './App.css';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [message, setMessage] = useState('❌');
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    axios.get('http://127.0.0.1:5000/api')
+      .then((response) => {
+        setMessage(response.data.message);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.error(error);
+        setLoading(false);
+      });
+  }, []);
+
   return (
-    <>hello world</>
+    <div>
+      <h3>Connected to Backend: {loading ? '🔄 ' : message}</h3>
+      <>hello world</>
+    </div>
   );
 }
 
