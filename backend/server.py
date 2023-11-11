354aefbaa2a23ea60db5651111b0ed7bd969e03c
georegehehe <wangg@uchicago.edu>
2023-11-11 13:14:41 -0600

    chat backend

--- backend/server.py
+++ backend/server.py
@@ -1,15 +1,37 @@
 from flask import Flask, request
 from flask_cors import CORS, cross_origin
+import pandas as pd 
+from openai import OpenAI
+import config
 
 app = Flask(__name__)
 
 cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
 
+client = OpenAI(api_key=config.API_KEY)
+
+
 @app.route('/api', methods=['GET'])
 def get_data():
     return {'message': '✅'}
 
-
+@app.route('/api/getInstruction', methods=['POST'])
+def get_instruction():
+    data = request.json
+    recipeName = data['recipeName']
+    ingredients = data['ingredients']
+    ingredient_text = ",".join(ingredients)
+    try:
+        prompt = f"Imagine you are in an advanced future space station and you now have to give instructions on how to cook space foods. The name of the space food is {recipeName}. The ingredients needed for the foods are {ingredient_text}. Be brief"
+        messages = [{"role": "user", "content": prompt}]
+        response = client.chat.completions.create(model = "gpt-3.5-turbo",
+        messages = messages,
+        temperature = 0)
+        print(response.choices[0].message.content)
+        return {'message': response.choices[0].message.content}
+    except Exception as e:
+        app.logger.error(f'error: {e}')
+        return "internal server error", 500
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -7,6 +7,18 @@ function App() {
   const [message, setMessage] = useState('❌');
   const [loading, setLoading] = useState(true);
 
+  const handleClick = () => {
+    //👇🏻 Send the SMS by calling the backend
+      axios.post('http://127.0.0.1:5000/api/getInstruction', {recipeName: "Space Tartar", ingredients: ['1.5 lbs of beef', '1 lemon', '2 tortilla']})
+      .then((response) => {
+        console.log(response);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+
+  };
+
   useEffect(() => {
     axios.get('http://127.0.0.1:5000/api')
       .then((response) => {
@@ -22,7 +34,9 @@ function App() {
   return (
     <>
         <h1>hello world</h1>
+        <h3>Connected to Backend: {loading ? '🔄 ' : message}</h3>
         <img src={headerImage} alt="food in space" className="headerImage"></img>
+        <button onClick={handleClick}>Click Me</button>
     </>
   );
 }
