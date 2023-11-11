511304bfcf2bb9d124dacbecb544e2a069cb216c
zephyroe <114773939+zroe1@users.noreply.github.com>
2023-11-11 11:57:19 -0600

    added header image/font

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,8 +1,12 @@
+import headerImage from "./foodImages/homepagebanner.png"
 import './App.css';
 
 function App() {
   return (
-    <>hello world</>
+    <>
+        <h1>hello world</h1>
+        <img src={headerImage} alt="food in space" className="headerImage"></img>
+    </>
   );
 }
 
